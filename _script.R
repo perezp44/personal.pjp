c0d66a942a602a007d3bdaf30178d245c17cd73b
Pedro J. Perez(H) <pedro.j.perez@uv.es>
2016-10-12 03:00:26 +0200

    a

--- _script.R
+++ _script.R
@@ -27,25 +27,26 @@ aa <- round_nice_pjp(0.00044, nn = 2 )   #- no funciona en df
 ?names_v_df_pjp
 ?val_unicos_df_pjp
 browseVignettes("personal.pjp")
-vignette("my_vignette")
+vignette("my_vignette.html")
 
-
+?browseVignettes
 
 
 #--- llevarlo a Github
-0) En Github crear un repo que se llame igual que tu package.
-1) en Git Bash te situas en la carpeta del package. en mi caso con:   cd c:/Users/perezp/Desktop/a_GIT_2016/personal.pjp
-2) Inicializas el repo vacio con :  `git init`
-3) Añades los archivos al repo: `git add .`  El punto indica que añades todos?
-4) Commit inicial: git commit --all --message "Creando el REPO"
-5) git remote add origin https://github.com/perezp44/personal.pjp.git
-6) git push -u origin master
+#0) En Github crear un repo que se llame igual que tu package.
+#1) en Git Bash te situas en la carpeta del package. en mi caso con:   cd c:/Users/perezp/Desktop/a_GIT_2016/personal.pjp
+#2) Inicializas el repo vacio con :  `git init`
+#3) Añades los archivos al repo: `git add .`  El punto indica que añades todos?
+#4) Commit inicial: git commit --all --message "Creando el REPO"
+#5) git remote add origin https://github.com/perezp44/personal.pjp.git
+#6) git push -u origin master
 
 #--------------- cargarlo desde Github
 
 
 
 devtools::install_github('perezp44/personal.pjp')
-  library(pkg4albacete)
+  library(personal.pjp)
 
+browseVignettes(package = "personal.pjp")
 
